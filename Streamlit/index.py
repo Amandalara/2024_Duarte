c7b754af60f85cf5532ecba946178785ec093001
Amanda Lara Duarte da Costa <161068129+Amandalara@users.noreply.github.com>
2024-10-11 18:54:48 +0000

    Aula DE streamlit

---
+++ Streamlit/index.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from RetanguloUI import RetanguloUI
+RetanguloUI.main()
+# as é usado para usar abreviações nesse caso para não chamar sempre o streamlit
+
+# st.header("POO em python com Streamlit")
+
+# if st.button("Clique Aqui"):
+#     st.write("Bem-vinda ao Streamlit!")
